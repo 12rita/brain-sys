13c2654b11fd8f2d0373cfce10c71eb27c93e3f7
Margo <m.goncharova@group-ib.com>
2024-10-16 23:46:35 +0300

    fix: style fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {ToasterProvider, WebSocketProvider} from "./hooks";
-import {Connect, Main} from "./pages";
-import {ERoutes} from "./routes.ts";
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ToasterProvider, WebSocketProvider } from './hooks';
+import { Connect, Main } from './pages';
+import { ERoutes } from './routes.ts';
 
 function App() {
-
-    return (
-        <>
-            <WebSocketProvider>
-                <BrowserRouter>
-                    <ToasterProvider>
-                        <Routes>
-                            <Route path={ERoutes.CONNECT} element={<Connect/>}/>
-                            <Route path={ERoutes.MAIN} element={<Main/>}/>
-                        </Routes>
-                    </ToasterProvider>
-                </BrowserRouter>
-            </WebSocketProvider>
-        </>
-    )
+  return (
+    <>
+      <WebSocketProvider>
+        <BrowserRouter>
+          <ToasterProvider>
+            <Routes>
+              <Route path={ERoutes.CONNECT} element={<Connect />} />
+              <Route path={ERoutes.MAIN} element={<Main />} />
+            </Routes>
+          </ToasterProvider>
+        </BrowserRouter>
+      </WebSocketProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
