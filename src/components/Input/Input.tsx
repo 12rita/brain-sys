fe25e894f6c0041538001227b59fbc2c44ea5a20
Margo <m.goncharova@group-ib.com>
2024-11-09 02:08:06 +0300

    feat: trim for name has been added

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,29 +1,36 @@
-import {ChangeEvent, FC, InputHTMLAttributes, useCallback, useState} from 'react'
+import { ChangeEvent, FC, InputHTMLAttributes, useCallback, useState } from 'react';
 import styles from './styles.module.css';
 
 interface IInput extends InputHTMLAttributes<HTMLInputElement> {
-    value: string
-    onChangeInput: (value: string) => void
-    className?: string;
+  value: string;
+  onChangeInput: (value: string) => void;
+  className?: string;
 }
 
-export const Input: FC<IInput> = ({value, onChangeInput, className, ...otherProps}) => {
+export const Input: FC<IInput> = ({ value, onChangeInput, className, ...otherProps }) => {
+  const [error, setError] = useState(false);
+  const handleChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      if (error) setError(false);
+      onChangeInput(e.target?.value?.trim());
+    },
+    [error, onChangeInput]
+  );
 
-    const [error, setError] = useState(false)
-    const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-        if (error) setError(false)
-        onChangeInput(e.target?.value)
-    }, [error, onChangeInput]);
+  const handleBlur = useCallback(() => {
+    if (!otherProps.pattern) return;
+    const regexp = new RegExp(otherProps.pattern);
+    const isValid = regexp.test(value);
+    if (!isValid) setError(true);
+  }, [otherProps.pattern, value]);
 
-    const handleBlur = useCallback(() => {
-        if (!otherProps.pattern) return;
-        const regexp = new RegExp(otherProps.pattern);
-        const isValid = regexp.test(value);
-        if (!isValid) setError(true);
-    }, [otherProps.pattern, value])
-
-    return (
-        <input className={`${className} ${error ? styles.error : ''}`} onBlur={handleBlur} value={value} onChange={handleChange} {...otherProps} />
-    )
-}
-
+  return (
+    <input
+      className={`${className} ${error ? styles.error : ''}`}
+      onBlur={handleBlur}
+      value={value}
+      onChange={handleChange}
+      {...otherProps}
+    />
+  );
+};
